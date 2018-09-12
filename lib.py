ee0b243906b4082d647cb66293b24a7314a83cc6
Alexey Rudnikevich <rudnikevich.ai@gmail.com>
2018-09-12 15:55:24 +0300

    tictactoe 1

--- lib.py
+++ lib.py
@@ -40,3 +40,47 @@ def is_palindrome (text) :
     if (text == text[::-1]):
         return True
     return False
+
+def find_winner(game_state):
+    '''
+    Determination of the winner in the game of tic-tac-toe
+    :param game_state:
+    :return:'X' or 'O'
+    '''
+    res2 = [0, 0, 0]
+    res3 = 'D'
+    res4, res5 = 0, 0
+    for r, i in enumerate(s):
+        res1 = 0
+        for c, j in enumerate(i):
+            if j == 'X':
+                res1 += 1
+                res2[c] += 1
+            if r == 0 and c == 0 and j == 'X': res4 += 1
+            if r == 0 and c == 2 and j == 'X': res5 += 1
+            if r == 1 and c == 1 and j == 'X': res4 += 1; res5 += 1
+            if r == 2 and c == 0 and j == 'X': res5 += 1
+            if r == 2 and c == 2 and j == 'X': res4 += 1
+            print(res1, end=" ")
+        print(res3, res2, res4, res5, end="  ")
+        if res1 == 0:
+            res3 = 'O'
+        elif res1 == 3:
+            res3 = 'X'
+    res = 'D'
+    if res3 == 'O': res = 'O'
+    elif res3 == 'X': res = 'X'
+    else:
+        if 0 in res2: res = 'O'
+        elif 3 in res2: res = 'X'
+        else:
+            if res4 == 0 or res5 == 0: res = 'O'
+            if res4 == 3 or res5 == 3: res = 'X'
+    return(res)
+
+
+    [
+        "OOX",
+        "XXO",
+        "OXX"
+    ]
