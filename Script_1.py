edca42aebce8d59f05bb8cc22fea2b3f36316e96
Alexey Rudnikevich <rudnikevich.ai@gmail.com>
2018-09-13 17:28:36 +0300

    Script_1 2018.09.13-1

--- Script_1.py
+++ Script_1.py
@@ -1,9 +1,11 @@
 import lib
 
 def main():
-    text = input('Введите текст (только буквы латинского алфавита и цифры)\n')
 
-    print('\nСамые часто встречающиеся символы :', lib.most_frequent_characters(text))
+    assert lib.most_frequent_characters("12233344445555566666777777788888888999999999")=='9'
+    assert lib.most_frequent_characters("q`112436456879809-0=-@#$%^&*()?=]p\|\/.,\\\\\\\\\\\\\\\whkhv l'n  jlnll")=='\\'
+
+    print(lib.most_frequent_characters("q`112436456879809-0=-=]p\|\/.,\\\\\\\\\\\\\\\\\whkhv l'n  jlnl   l"))
 
 
 if __name__ == '__main__':
